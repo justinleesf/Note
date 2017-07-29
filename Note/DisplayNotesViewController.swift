0601a505439e327e235238e4761833c02ec643cd
justinleesf <justinleesf@gmail.com>
2017-07-28 22:50:44 -0700

    Begin data modeling

---
+++ Note/DisplayNotesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DisplayNotesViewController.swift
+//  Note
+//
+//  Created by King Justin on 7/27/17.
+//  Copyright © 2017 toBeDetermined. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
---
+++ Note/ListNotesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListNotesTableViewCell.swift
+//  Note
+//
+//  Created by King Justin on 7/27/17.
+//  Copyright © 2017 toBeDetermined. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var modificationTimeLabel: UILabel!
+
+
+}
